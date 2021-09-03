38abfe90b4d5bd88bb11b1515471e51dcdc9a80b
PETER_ KIPTOO <peterkiptoo282@gmail.com>
2021-09-03 15:46:59 +0300

    initial commit

---
+++ UnitTestBigIntExtenderLibrary/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Exercise01;
+
+namespace UnitTestBigIntExtenderLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private BigIntExtenderLibrary extender;
+
+        [TestMethod]
+        public void TestValidInput()
+        {
+            string number = "18000000";
+            number = BigIntExtenderLibrary.Towards(double.Parse(number));
+
+            Assert.IsNotNull(number,"18000000");
+
+        }
+
+
+       
+
+    }
+}
